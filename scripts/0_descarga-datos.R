a1a3897980d1ec3daf1365b61b4ca9adb440c489
lauskuba <lauskuba@gmail.com>
2023-05-19 21:13:17 -0300

    actualizo_scripts, dropHipotesis.Rmd

--- scripts/0_descarga-datos.R
+++ scripts/0_descarga-datos.R
@@ -4,7 +4,7 @@
 #https://datos.gob.ar/dataset/salud-vacunas-contra-covid-19-dosis-aplicadas-republica-argentina---registro-desagregado
 #https://sisa.msal.gov.ar/datos/descargas/covid-19/files/datos_nomivac_covid19.zip
 
-options(timeout = max(3000, getOption("timeout")))
+options(timeout = max(3600, getOption("timeout")))
 
 download.file(url = "http://datos.salud.gob.ar/dataset/22445914-15aa-4169-acbf-5dafff74b592/resource/47d25d2c-57fd-4fc3-b09b-64b12d96563f/download/2022-10-31-vacunas-precios.xlsx",
               
--- scripts/0_importa-datos.R
+++ scripts/0_importa-datos.R
@@ -7,7 +7,9 @@ library(readr)
 file_path <<- "datos/datos_nomivac_covid19.csv"
 
 # Importo unicamente las X filas que quiero de ese archivo
-data <- read_csv(file_path, n_max = 1000)
+#data <- read_csv(file_path, n_max = 1000000)
+#data <- read_csv(file_path, n_max = 115605333)
+data <- read_csv(file_path)
 
 # Visualizo los datos
 # head(data)
